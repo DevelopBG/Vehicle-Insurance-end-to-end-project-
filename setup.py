cc49356092622e37620b3864024d3c3c4c89d526
DevelopBG <bani.ghosh10@gmail.com>
2025-08-30 16:06:21 +1000

    pushing done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Banibrata Ghosh",
+    author_email="bani.ghosh10@gmail.com",
+    packages=find_packages()
+)
